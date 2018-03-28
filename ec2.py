1c84d89290d0622e74146d12af39933b34c353a4
ganesshi <ganesshi8087@gmail.com>
2018-03-28 11:22:23 +0530

    first commit in ec2 branch

---
+++ ec2.py
@@ -0,0 +1,7 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+print (ec2.describe_vpcs()['Vpcs'][0]['VpcId'])
+print (ec2.describe_vpcs()['Vpcs'][1]['VpcId'])
+print (ec2.describe_vpcs()['Vpcs'][2]['VpcId'])
